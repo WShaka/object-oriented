c7aee89d8ef2cf4a3b3b69d7b5437f061edd6453
米 艾 <747732552@qq.com>
2020-10-05 21:21:00 +0800

    调用静态和非静态函数

--- 类的声明/Program.cs
+++ 类的声明/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace 类的声明
 {
+    class MyHelloWorld
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void CSayHello()
+        {
+            Console.WriteLine("你好！");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-           
+            /////静态的方法，无需实例化。在编译已经分配空间，编译完成后释放
+            MyHelloWorld.SayHello();
+            ////非静态，需要将类实例化形成对象。然后调用
+            MyHelloWorld myHelloWorld = new MyHelloWorld();
+            myHelloWorld.CSayHello();
+            /////释放对象的内存空间
+            myHelloWorld = null;
+            Console.ReadKey();
         }
     }
 }
